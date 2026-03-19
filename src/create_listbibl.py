6550d51a451bdf215ac6ccdea72795447c3b49f1
csae8092 <p.andorfer@gmail.com>
2026-03-19 11:47:10 +0100

    wip: script to create listorg

---
+++ src/create_listbibl.py
@@ -0,0 +1,51 @@
+import os
+from acdh_tei_pyutils.tei import TeiReader
+from acdh_tei_pyutils.utils import normalize_string
+
+data_dir = os.path.join("data", "indices")
+input_file = os.path.join(data_dir, "listbibl.xml")
+output_file = os.path.join(data_dir, "listorg.xml")
+
+template = """
+<?xml-model href="http://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_all.rng" type="application/xml" schematypens="http://relaxng.org/ns/structure/1.0"?>
+<?xml-model href="http://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_all.rng" type="application/xml"
+	schematypens="http://purl.oclc.org/dsdl/schematron"?>
+<TEI xmlns="http://www.tei-c.org/ns/1.0" xml:id="listorg.xml">
+    <teiHeader>
+        <fileDesc>
+            <titleStmt>
+                <title level="a">Ortsverzeichnis</title>
+                <title level="s">Eine Karl Kraus Bibliographie, basierend auf S.P. Scheichls "Kommentierte Auswahlbibliographie" (KAB)</title>
+                <editor ref="https://d-nb.info/gnd/1036708799">Bernhard Oberreither</editor>
+            </titleStmt>
+            <publicationStmt>
+                <p>Publication Information</p>
+            </publicationStmt>
+            <sourceDesc>
+                <p>Information about the source</p>
+            </sourceDesc>
+        </fileDesc>
+    </teiHeader>
+    <text>
+        <body>
+            <listOrg></listOrg>
+        </body>
+    </text>
+</TEI>
+"""
+
+items = set()
+doc = TeiReader(input_file)
+for x in doc.any_xpath(".//tei:bibl[@xml:id]/tei:publisher"):
+    items.add(normalize_string(x.text))
+
+orgs = {}
+for i, x in enumerate(sorted(list(items)), start=1):
+    orgs[x] = f"kab-org-{i:0>5}"
+
+
+for x in doc.any_xpath(".//tei:bibl[@xml:id]/tei:publisher"):
+    item = normalize_string(x.text)
+    item_id = orgs[item]
+    x.attrib["key"] = f"#{item_id}"
+
